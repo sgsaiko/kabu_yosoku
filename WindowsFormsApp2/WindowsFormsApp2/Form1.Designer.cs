ebba7b79d17e4a65aeb844585c194cd461dc4267
sgsaiko <35831769+sgsaiko@users.noreply.github.com>
2018-01-26 16:41:00 +0900

    初回

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtMeigara = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1578, 583);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txtMeigara
+            // 
+            this.txtMeigara.Location = new System.Drawing.Point(141, 12);
+            this.txtMeigara.Name = "txtMeigara";
+            this.txtMeigara.Size = new System.Drawing.Size(100, 31);
+            this.txtMeigara.TabIndex = 2;
+            this.txtMeigara.Text = "5713";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(247, 12);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(130, 31);
+            this.txtStart.TabIndex = 3;
+            this.txtStart.Text = "2008/1/1";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(383, 12);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(130, 31);
+            this.txtEnd.TabIndex = 4;
+            this.txtEnd.Text = "2016/12/31";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(519, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(871, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 63);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "クリップボードコピー";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1602, 672);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.txtMeigara);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtMeigara;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public const double Invalid = 1E+100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int IntStart;
+                int IntEnd;
+                int IntDatePos;
+
+                const int IntMA1 = 5;
+                const int IntMA2 = 20;
+                const int IntMA3 = 60;
+                const int IntMA4 = 100;
+                const int IntMA5 = 300;
+
+                var price = new ActiveMarket.Prices();
+                var cal = new ActiveMarket.Calendar();
+
+                IntStart = cal.DatePosition(DateTime.Parse(txtStart.Text),1);
+                IntEnd = cal.DatePosition(DateTime.Parse(txtEnd.Text), -1);
+
+                price.ReadBegin = IntStart;
+                price.ReadEnd = IntEnd;
+
+                price.Read(txtMeigara.Text);
+                label1.Text = price.Name();
+
+                if (IntStart < price.ReadBegin)
+                {
+                    IntStart = price.ReadBegin;
+                }
+                if(IntEnd > price.ReadEnd)
+                {
+                    IntEnd = price.ReadEnd;
+                }
+
+                DataTable dt = new DataTable("tname");
+                dt.Columns.Add("Date", typeof(String));
+                dt.Columns.Add("WeekNum", typeof(Decimal));
+                dt.Columns.Add("Youbi", typeof(Decimal));
+                dt.Columns.Add("Open",typeof(Decimal));
+                dt.Columns.Add("High", typeof(Decimal));
+                dt.Columns.Add("Low", typeof(Decimal));
+                dt.Columns.Add("Close", typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA1.ToString(), typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA2.ToString(), typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA3.ToString(), typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA4.ToString(), typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA5.ToString(), typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA1.ToString() + "乖", typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA2.ToString() + "乖", typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA3.ToString() + "乖", typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA4.ToString() + "乖", typeof(Decimal));
+                dt.Columns.Add("MA" + IntMA5.ToString() + "乖", typeof(Decimal));
+                dt.Columns.Add("ZenSa", typeof(Decimal));
+                dt.Columns.Add("ZenHi", typeof(Decimal));
+
+                dt.AcceptChanges();
+
+                for (IntDatePos  = IntStart; IntDatePos <= IntEnd; IntDatePos++)
+                {
+                    if (price.IsClosed(IntDatePos) != 0)
+                    {
+                        //dataGridView1.Rows.Add(cal.Date(IntDatePos).ToString("yyyy/MM/dd"),"","","","");
+                    }
+                    else if(price.Close(IntDatePos) == 0 | price.Close(IntDatePos) == Invalid)
+                    {
+                        //dataGridView1.Rows.Add(cal.Date(IntDatePos).ToString("yyyy/MM/dd"), "", "", "", "");
+                    }
+                    else
+                    {
+                        //dataGridView1.Rows.Add(cal.Date(IntDatePos).ToString("yyyy/MM/dd")
+                        //    , 
+                        //                        price.Open(IntDatePos),
+                        //                        price.High(IntDatePos),
+                        //                        price.Low(IntDatePos),
+                        //                        price.Close(IntDatePos));
+
+                        DataRow drNewRow = dt.NewRow();
+                        drNewRow["Date"] = cal.Date(IntDatePos).ToString("yyyy/MM/dd");
+
+                        DateTime calDate = new DateTime(cal.Date(IntDatePos).Year,cal.Date(IntDatePos).Month,cal.Date(IntDatePos).Day);
+                        Calendar calender = CultureInfo.CurrentCulture.Calendar;
+
+                        drNewRow["Youbi"] = (int)calDate.DayOfWeek;
+                        drNewRow["WeekNum"] = calender.GetWeekOfYear(calDate,CalendarWeekRule.FirstDay,DayOfWeek.Sunday);
+             
+                        drNewRow["Open"] = price.Open(IntDatePos);
+                        drNewRow["High"] = price.High(IntDatePos);
+                        drNewRow["Low"] = price.Low(IntDatePos);
+                        drNewRow["Close"] = price.Close(IntDatePos);
+
+                        drNewRow["MA" + IntMA1.ToString()] = 0;
+                        drNewRow["MA" + IntMA2.ToString()] = 0;
+                        drNewRow["MA" + IntMA3.ToString()] = 0;
+                        drNewRow["MA" + IntMA4.ToString()] = 0;
+                        drNewRow["MA" + IntMA5.ToString()] = 0;
+
+                        drNewRow["MA" + IntMA1.ToString() + "乖"] = 0;
+                        drNewRow["MA" + IntMA2.ToString() + "乖"] = 0;
+                        drNewRow["MA" + IntMA3.ToString() + "乖"] = 0;
+                        drNewRow["MA" + IntMA4.ToString() + "乖"] = 0;
+                        drNewRow["MA" + IntMA5.ToString() + "乖"] = 0;
+
+                        drNewRow["ZenSa"] = 0;
+                        drNewRow["ZenHi"] = 0;
+
+                        dt.Rows.Add(drNewRow);
+                    }
+                }
+                int i = 0;
+                int j = 0;
+                decimal decMA1 = 0;
+                decimal decMA2 = 0;
+                decimal decMA3 = 0;
+                decimal decMA4 = 0;
+                decimal decMA5 = 0;
+
+                decimal decZenSa = 0;
+                decimal decZenHi = 0;
+
+                for (i=0;i < dt.Rows.Count; i++)
+                {
+                    if(i > 0)
+                    {
+                        decZenSa = decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        decZenSa -= decimal.Parse(dt.Rows[i-1]["Close"].ToString());
+                        dt.Rows[i]["ZenSa"] = decZenSa;
+
+                        decZenHi = decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        decZenHi = decZenHi / decimal.Parse(dt.Rows[i - 1]["Close"].ToString());
+                        dt.Rows[i]["ZenHi"] = decZenHi;
+                    }
+                    if(i >= IntMA1)
+                    {
+                        decMA1 = 0;
+                        for(j = 0;j < IntMA1; j++)
+                        {
+                            decMA1 += decimal.Parse(dt.Rows[i-j]["Close"].ToString());
+                        }
+                        decMA1 = decMA1 / IntMA1;
+                        dt.Rows[i]["MA" + IntMA1.ToString()] = decMA1;
+
+                        decMA1 = decimal.Parse(dt.Rows[i]["Close"].ToString()) - decMA1;
+                        decMA1 = decMA1 / decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        dt.Rows[i]["MA" + IntMA1.ToString() + "乖"] = decMA1 * 100;
+                    }
+                    if (i >= IntMA2)
+                    {
+                        decMA2 = 0;
+                        for (j = 0; j < IntMA2; j++)
+                        {
+                            decMA2 += decimal.Parse(dt.Rows[i-j]["Close"].ToString());
+                        }
+                        decMA2 = decMA2 / IntMA2;
+                        dt.Rows[i]["MA" + IntMA2.ToString()] = decMA2;
+
+                        decMA2 = decimal.Parse(dt.Rows[i]["Close"].ToString()) - decMA2;
+                        decMA2 = decMA2 / decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        dt.Rows[i]["MA" + IntMA2.ToString() + "乖"] = decMA2 * 100;
+                    }
+                    if (i >= IntMA3)
+                    {
+                        decMA3 = 0;
+                        for (j = 0; j < IntMA3; j++)
+                        {
+                            decMA3 += decimal.Parse(dt.Rows[i-j]["Close"].ToString());
+                        }
+                        decMA3 = decMA3 / IntMA3;
+                        dt.Rows[i]["MA" + IntMA3.ToString()] = decMA3;
+
+                        decMA3 = decimal.Parse(dt.Rows[i]["Close"].ToString()) - decMA3;
+                        decMA3 = decMA3 / decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        dt.Rows[i]["MA" + IntMA3.ToString() + "乖"] = decMA3 * 100;
+                    }
+                    if (i >= IntMA4)
+                    {
+                        decMA4 = 0;
+                        for (j = 0; j < IntMA4; j++)
+                        {
+                            decMA4 += decimal.Parse(dt.Rows[i-j]["Close"].ToString());
+                        }
+                        decMA4 = decMA4 / IntMA4;
+                        dt.Rows[i]["MA" + IntMA4.ToString()] = decMA4;
+
+                        decMA4 = decimal.Parse(dt.Rows[i]["Close"].ToString()) - decMA4;
+                        decMA4 = decMA4 / decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        dt.Rows[i]["MA" + IntMA4.ToString() + "乖"] = decMA4 * 100;
+                    }
+                    if (i >= IntMA5)
+                    {
+                        decMA5 = 0;
+                        for (j = 0; j < IntMA5; j++)
+                        {
+                            decMA5 += decimal.Parse(dt.Rows[i-j]["Close"].ToString());
+                        }
+                        decMA5 = decMA5 / IntMA5;
+                        dt.Rows[i]["MA" + IntMA5.ToString()] = decMA5;
+
+                        decMA5 = decimal.Parse(dt.Rows[i]["Close"].ToString()) - decMA5;
+                        decMA5 = decMA5 / decimal.Parse(dt.Rows[i]["Close"].ToString());
+                        dt.Rows[i]["MA" + IntMA5.ToString() + "乖"] = decMA5 * 100;
+                    }
+                }
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(dataGridView1.GetClipboardContent());
+        }
+    }
+}
